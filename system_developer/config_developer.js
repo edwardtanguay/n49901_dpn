bb3f0feef557917018acf4ee76c14d02753aef6c
Edward Tanguay (wk.win) <edward.tanguay@gefegmbh.local>
2019-11-25 15:06:47 +0100

    config_developer / gitignore fix

---
+++ system_developer/config_developer.js
@@ -0,0 +1,13 @@
+"use strict"
+const qstr = require('../qtools/qstr');
+const qdev = require('../qtools/qdev');
+const config = require('../system/config');
+
+
+// outputs debugging information to console, keeps developer logged in even after backend modifications to code
+exports.developing = function () {
+	return false;//DYNAMIC_LINE:configDeveloping
+}
+exports.defaultUserIdCode = function () {
+	return 'anonymousUser';//DYNAMIC_LINE:defaultUserIdCode
+}
